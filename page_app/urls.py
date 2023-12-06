ee506747fd0a1471a0ada19a45ce2904ece93cbd
samuka <150863265+samukamf10@users.noreply.github.com>
2023-12-05 22:33:42 -0300

    atualizado e corrigido

--- page_app/urls.py
+++ page_app/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from page_app.views import index, contato
+from page_app.views import index, contato, footer, header, home, services
 
 urlpatterns = [
     # Cadastrar URLs aqui
     path('', index),
 
     path('contato/', contato),
+    path('footer/', footer),
+    path('header/', header),
+    path('home/', home), 
+    path('services/', services),
 ]
--- page_app/views.py
+++ page_app/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
-def index (request):
+def home (request):
     return render(request,"page_app/partial/home.html")
 
 def contato (request):
     return render(request, "page_app/partial/contato.html")
+
+def welcome (request):
+    return render(request, "page_app/partial/welcome.html")
+
+def footer (request):
+    return render(request, "page_app/partial/footer.html")
+
+def header (request):
+    return render(request, "page_app/partial/header.html")
+
+def services (request):
+    return render(request, "page_app/partial/services.html")
+
+def index (request):
+    return render(request,"page_app/global/index.html")
